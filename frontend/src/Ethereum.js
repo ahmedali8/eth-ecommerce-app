2fb43fff436726462ff1a8d26701d0b313b68391
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-13 23:18:40 +0500

    added Ethereum.js to get blockchain data

---
+++ frontend/src/Ethereum.js
@@ -0,0 +1,38 @@
+import { ethers, Contract } from 'ethers';
+import PaymentProcessor from './contracts/PaymentProcessor.json';
+import Dai from './contracts/Dai.json';
+
+const getBlockchain = () =>
+  new Promise((resolve, reject) => {
+    window.addEventListener('load', async () => {
+      if (window.ethereum) {
+        await window.ethereum.enable();
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const signerAddress = await signer.getAddress();
+
+        const paymentProcessor = new Contract(
+          PaymentProcessor.networks[window.ethereum.networkVersion].address,
+          PaymentProcessor.abi,
+          signer
+        );
+
+        const dai = new Contract(
+          //for mainnet and public testnet replace by address of already deployed dai token
+          Dai.networks[window.ethereum.networkVersion].address,
+          Dai.abi,
+          signer
+        );
+
+        resolve({ provider, paymentProcessor, dai });
+      }
+
+      resolve({
+        provider: undefined,
+        paymentProcessor: undefined,
+        dai: undefined,
+      });
+    });
+  });
+
+export default getBlockchain;
